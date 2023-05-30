655bc706eff45f773a46b9dca0c0bd7b05b74cb7
DarioGiusino <dario.giusino@gmail.com>
2023-05-30 16:32:17 +0200

    added validation

--- src/main/java/org/java/expizza/controller/PizzaController.java
+++ src/main/java/org/java/expizza/controller/PizzaController.java
@@ -8,11 +8,14 @@ import org.java.expizza.serv.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class PizzaController {
@@ -52,13 +55,23 @@ public class PizzaController {
 	}
 	
 	@GetMapping("/pizza/create")
-	public String create() {
+	public String create(Model model) {
+		
+		model.addAttribute("pizza", new Pizza());
 		
 		return "pizza/create";
 	}
 	
 	@PostMapping("/pizza/store")
-	public String store(@ModelAttribute Pizza pizza){
+	public String store(@Valid @ModelAttribute Pizza pizza, BindingResult bindingResult, Model model){
+		
+		if (bindingResult.hasErrors()) {
+			
+			model.addAttribute("pizza", pizza);
+			model.addAttribute("errors", bindingResult);
+			
+			return "pizza/create";
+		}
 		
 		pizzaService.save(pizza);
 		
@@ -77,7 +90,15 @@ public class PizzaController {
 	}
 	
 	@PostMapping("pizza/update/{id}")
-	public String update(@PathVariable("id") int id, @ModelAttribute Pizza pizza) {
+	public String update(@PathVariable("id") int id, @Valid @ModelAttribute Pizza pizza, BindingResult bindingResult, Model model) {
+		
+		if (bindingResult.hasErrors()) {
+			
+			model.addAttribute("pizza", pizza);
+			model.addAttribute("errors", bindingResult);
+			
+			return "pizza/edit";
+		}
 		
 		pizzaService.save(pizza);
 		
--- src/main/java/org/java/expizza/pojo/Pizza.java
+++ src/main/java/org/java/expizza/pojo/Pizza.java
@@ -5,6 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Pizza {
@@ -12,6 +16,8 @@ public class Pizza {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
+	@NotBlank(message = "Please, insert a name.")
+	@Size(min = 3, max = 25, message = "The name must be 3-25 char.")
 	private String name;
 	
 	@Column(columnDefinition="TEXT")
@@ -19,6 +25,8 @@ public class Pizza {
 	
 	private String picture;
 	
+	@NotNull(message = "Please, insert a price.")
+	@Min(value = 1, message = "Min price is 1.")
 	private float price;
 
 	public Pizza() {}
